cf30aa570c1da0da067be6c911f625cfd57bd334
Jhon Zuñiga <zuni230669>
2021-09-13 23:06:40 -0500

    Exercise sum service.

--- generate.sh
+++ generate.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 protoc greet/greetpb/greet.proto --go_out=plugins=grpc:.
+#!/mnt/c/Users/zuni230xxx/go/src/github.com/jhonzp/grpc-go-course
+protoc calculator/calculator_pb/calculator.proto --go_out=plugins=grpc:.
